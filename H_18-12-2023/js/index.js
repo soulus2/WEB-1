e5fafdb7085015710a904d94c4e2b9aa446d2eda
soulus2 <73872349+soulus2@users.noreply.github.com>
2024-01-11 21:19:39 +0400

    finaly it works

--- H_18-12-2023/js/index.js
+++ H_18-12-2023/js/index.js
@@ -1,7 +1,6 @@
 const colors=["r","g","b","y"];
 let isStart=true;
 let TEAMS=[];
-let pairs=[];
 function putBlock(i,j,id) {
     document.getElementById(id).innerHTML+=`<div class="${colors[i]} teams" id="${colors[i]}${j}"><p>${i}X${j}</p></div>`;
   
@@ -11,40 +10,50 @@ function rand(max){
   return Math.floor(Math.random() * max); 
 }
 
-function group() {
-  let teams = TEAMS;
-  let pairs=[];
-
-    let rl =[];
-  for(let i=0;i<teams.length;i++){
-    if (teams[i]==0) continue;
-    let ri=0;
-    let test =true;
-    // console.log(teams.length);
-
-    while (test) {
-      ri=rand(teams.length);
-      let skip=false;
-      for(let r=0;r<rl.length;r++){
-        if (ri==rl[r]) {skip=true; break;}
-      }
-      if (skip) continue;
-      rl.push(""+ri);
-      
-      console.log(ri);
-      console.log(teams[ri][0]);
-      // break;
-      console.log(rl);
-
-      pairs.push([teams[i],teams[ri]]);
-      teams[ri]=0;
-      teams[i]=0;
-      break;
-      
+function moveToBack(array, index) {
+    if (index < 0 || index >= array.length) {
+        console.error("Index out of bounds");
+        return;
     }
+
+    const element = array.splice(index, 1)[0];
+    array.push(element);
+}
+function displayPairs(pairs){
+  document.getElementById("grid").innerHTML = '';
+  for (let p=0;p<pairs.length;p++){
+    if (pairs[p]==null) continue;
+    document.getElementById("grid").innerHTML+='<dev id="pair'+p+'" class="pairCont">'
+    putBlock(pairs[p][0][0],pairs[p][0][1],"pair"+p);
+    putBlock(pairs[p][1][0],pairs[p][1][1],"pair"+p);
+    document.getElementById("grid").innerHTML+="</div>"
+  }
+}
+
+function group() {
+  let teams=[...TEAMS];
+  let pairs=[];
+  console.log("teams at start: ",teams);
+  while (teams.length>2){
+    let r1 = rand(teams.length);
+    let r2 = rand(teams.length);
+    console.log(teams[r1],teams[r2]);
+    if(teams[r1][0]==teams[r2][0]) continue;
+    pairs.push([teams[r1],teams[r2]]); 
+    teams.splice(r1,1);
+    teams.splice(r2,1);
   }
   
-  // console.log(pairs);
+
+  console.log("teams at end: ",teams);
+  displayPairs(pairs);
+  TEAMS=[];
+  for (let p=0;p<pairs.length;p++){
+    let rp = rand(2);
+    TEAMS.push(pairs[p][rp]);
+  }
+  console.log("TEAMS in end: ",TEAMS)
+  
 }
 
 function makeGrid() {
@@ -54,7 +63,7 @@ function makeGrid() {
   for (let i=0;i<4;i++){
     for(let j=0;j<4;j++){
       putBlock(i,j,"grid");
-      TEAMS.push(i+""+j);
+      TEAMS.push([i,j]);
     }
   }  
 }
